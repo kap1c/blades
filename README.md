069f8f9e21c692cf41b871a104261c945cb55037
galymsarsebek <ggkapic@mail.ru>
2024-11-13 21:45:32 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Change theme with select option
---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index(Request $request)
+    {
+        $theme = $request->theme !== null ? $request->theme : 'classic';
+
+        return view($theme, ['theme' => $theme]);
+    }
+}
